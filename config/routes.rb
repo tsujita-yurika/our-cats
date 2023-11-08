0f5c57172af4ebf3e34fd0ed8f5264571ed59a7e
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2023-11-08 02:19:41 +0000

    [導入]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+
+# 会員用
+# URL /customers/sign_in ...
+devise_for :members,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+
+# 会員用
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+    get 'members/index' => 'members#index'
+    get 'members/show' => 'members#show'
+    get 'members/edit' => 'members#edit'
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
