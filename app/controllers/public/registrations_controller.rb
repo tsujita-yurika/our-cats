bf73238b93bafcaa2104495e359befa7704ef37b
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2023-12-20 03:09:47 +0000

    エラー修正

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -4,6 +4,8 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  
+
   protected
 
   def after_sign_up_path_for(resource)
